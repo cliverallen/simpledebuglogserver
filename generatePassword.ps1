ffcc1e846eced3bdd2bea0627af6e827a1a3ea61
Clive <cliverallen@gmail.com>
2021-03-14 20:47:51 +0000

    alpha test 1

--- generatePassword.ps1
+++ generatePassword.ps1
@@ -1,2 +1,3 @@
 $Password = ([char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126)) + 0..9 | sort-object {Get-Random})[0..8] -join ''
 ((Get-Content -path ./pwsh/web-pages-docker.ps1 -Raw) -replace '###PASS###',$Password) | Set-Content -Path ./pwsh/web-pages-docker.ps1
+Write-Host "Password set to " $Password
