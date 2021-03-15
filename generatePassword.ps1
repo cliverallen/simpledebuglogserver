be277563d4f005055bbbcdf08d1f95de0eb9addf
Clive <cliverallen@gmail.com>
2021-03-15 10:31:13 +0000

    alpha test 8

--- generatePassword.ps1
+++ generatePassword.ps1
@@ -1,5 +1,5 @@
 $currentPath = Get-Location
-$Password = ([char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126)) + 0..9 | sort-object {Get-Random})[0..8] -join ''
+$Password = ([char[]]([char]65..[char]90) + [char[]]([char]97..[char]122) + 0..9 | sort-object {Get-Random})[0..8] -join ''
 ((Get-Content -path $currentPath/pwsh/web-pages-docker.ps1 -Raw) -replace '###PASS###',$Password) | Set-Content -Path ./pwsh/web-pages-docker.ps1
 Write-Host "Current dir " $currentPath
 Write-Host "Password set to " $Password
