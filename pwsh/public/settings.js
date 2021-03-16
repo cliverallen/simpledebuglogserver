5c93d35bfcb432289c30dae75b774d8fdb198ba0
Clive <cliverallen@gmail.com>
2021-03-16 13:00:34 +0000

    added example code

--- pwsh/public/settings.js
+++ pwsh/public/settings.js
@@ -1,33 +1,16 @@
 (function (window, document) {
 
     document.getElementById("showToken").onclick = showToken;
-    // document.getElementById("updateToken").onclick = updateToken;
     
-    function updateToken() {
-        var status = ""
-        if(document.getElementById("tokenactive").checked) {
-            status = "on"
-        }
-        var query = "tokenactive=" + status + "&tokencode=" + document.getElementById("tokenCode").value;
-        $url = "/updatesettings?" + query;
-        $('#fake').load($url);
-        //var jqxhr = $.ajax(url);
-        // const Http = new XMLHttpRequest();
-        // setTimeout(() => {  console.log("World!"); }, 500);
-        // const url='https://jsonplaceholder.typicode.com/posts';
-        // Http.open("GET", url);
-        // Http.send();
-
-        // Http.onreadystatechange = (e) => {
-        //    console.log(Http.responseText)
-        //}
-    }
     function showToken() {
-        var x = document.getElementById("tokenCode");
-        if (x.type === "password") {
-          x.type = "text";
+        var tc = document.getElementById("tokenCode");
+        var cu = document.getElementById("codeusage");
+        if (tc.type === "password") {
+          tc.type = "text";
+          cu.style.display = "none";
         } else {
-          x.type = "password";
+          tc.type = "password";
+          cu.style.display = "block";
         }
     }
     
